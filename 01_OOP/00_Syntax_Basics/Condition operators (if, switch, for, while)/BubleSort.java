cca30621672635ad047f49b567132cab24e42699
Yevhen Kariev <mediams@mail.ru>
2024-09-30 21:25:26 +0200

    30-09-2024 21:25 from PC 'Yevhen Kariev'

--- 01_OOP/00_Syntax_Basics/Condition operators (if, switch, for, while)/BubleSort.java
+++ 01_OOP/00_Syntax_Basics/Condition operators (if, switch, for, while)/BubleSort.java
@@ -2,19 +2,29 @@ import java.util.Arrays;
 
 public class BubleSort {
     public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 4, 7, 3, 1, 5, 9, 8, 4};
+        int[] array = new int[]{9, 2, 4, 7, 3, 1, 5, 1, 8, 4};
         System.out.println(Arrays.toString(array));
 
 
         getSort(array);
         System.out.println(Arrays.toString(array));
 
+        array = new int[]{1, 2, 4, 7, 3, 1, 5, 9, 8, 4};
+        getSortWithBoolean(array);
+        System.out.println(Arrays.toString(array));
+
+        array = new int[]{1, 2, 4, 7, 3, 1, 5, 9, 8, 4};
+        getSortWithBooleanAndMinus(array);
+        System.out.println(Arrays.toString(array));
+
 
     }
 
     public static int[] getSort(int[] array) {
+
         int count = 0;
         for (int i = 0; i < array.length - 1; i++) {
+            System.out.println(Arrays.toString(array) + "loop for 1");
             count++;
             int temp = 0;
             for (int j = 0; j < array.length - 1; j++) {
@@ -23,7 +33,55 @@ public class BubleSort {
                     temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
+                    System.out.println(Arrays.toString(array) + "loop for 2");
                 }
+            }
+        }
+        System.out.println(count);
+        return array;
+    }
+
+    public static int[] getSortWithBoolean(int[] array) {
+        boolean check = false;
+        int count = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            check = true;
+            count++;
+            int temp = 0;
+            for (int j = 0; j < array.length - 1; j++) {
+                count++;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    check = false;
+                }
+            }
+            if (check) break;
+        }
+        System.out.println(count);
+        return array;
+    }
+
+    public static int[] getSortWithBooleanAndMinus(int[] array) {
+        boolean check = false;
+        int count = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            check = true;
+            count++;
+            int temp = 0;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                count++;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    check = false;
+                }
+
+            }
+            if (check) {
+                break;
             }
         }
         System.out.println(count);
