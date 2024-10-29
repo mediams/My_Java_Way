32115cd57f1ebdc6e805f33c6e6b0c972ee4e5ed
Yevhen Kariev <mediams@mail.ru>
2024-10-29 23:24:52 +0100

    29-10-2024 23:24 from PC 'Yevhen Kariev'

--- 02_Collections_Framework/00_Collections/Standard collections from Java library (ArrayList, HashMap, HashSet, LinkedList and others)/set/BookEx.java
+++ 02_Collections_Framework/00_Collections/Standard collections from Java library (ArrayList, HashMap, HashSet, LinkedList and others)/set/BookEx.java
@@ -12,15 +12,14 @@ public class BookEx {
         books.add(new Book(new BookData("Book 1", "Tom Smith"), false, 21.0, 8));
 
         Collections.sort(books); //TimSort  O(n log n).
-        sortComparator(books, new Book.BookComparatorByPrice());
+        sortComparator(books, new Book.BookComparatorByPrice()); //O(n^2)
         System.out.println(books);
     }
 
     public static void sortComparator(List<Book> books, Comparator<Book> comparator) {//O(n^2)
         Book temp = null;
-        boolean isSort = true;
         for (int i = 0; i < books.size(); i++) {
-            isSort = true;
+            boolean isSort = true;
             for (int j = 0; j < books.size() - i - 1; j++) {
                 if (comparator.compare(books.get(j), books.get(j + 1)) > 0) {
                     temp = books.get(j);
