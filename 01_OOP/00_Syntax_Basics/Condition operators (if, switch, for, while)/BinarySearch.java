a385b25ffee8b8f2ff1e02b7a67f0ac0cc6cd775
Yevhen Kariev <mediams@mail.ru>
2024-10-31 18:51:08 +0100

    31-10-2024 18:51 from PC 'Yevhen Kariev'

--- 01_OOP/00_Syntax_Basics/Condition operators (if, switch, for, while)/BinarySearch.java
+++ 01_OOP/00_Syntax_Basics/Condition operators (if, switch, for, while)/BinarySearch.java
@@ -1,23 +1,23 @@
 public class BinarySearch {
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int element = 2;
+        int element = 9;
         System.out.println(searchBinary(array, element));
     }
 
     private static int searchBinary(int[] array, int element) {
         int left = 0;
-        int right = array.length;
+        int right = array.length - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (array[mid] == element) {
                 return mid;
             } else if (array[mid] < element) {
-                right = mid - 1;
-            }
+                left = mid + 1;
+            } else right = mid - 1;
 
-        System.out.println("mid = " + mid);
+            System.out.println("mid = " + mid);
         }
         return 0;
     }
