e0decc16087c5ded96cb4bd1aaad717fa285a081
Yevhen Kariev <mediams@mail.ru>
2024-12-22 17:32:56 +0100

    22-12-2024 17:32 from PC 'Yevhen Kariev'

---
+++ 06_Multithreading/Intro_Multithreading_MainProblems/TaskEx/Message.java
@@ -0,0 +1,24 @@
+package TaskEx;
+
+public class Message {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
