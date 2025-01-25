c08d68d9d59b559c9db32d3ae522c72a262df42c
Yevhen Kariev <mediams@mail.ru>
2025-01-25 15:24:23 +0100

    25-01-2025 15:24 from PC 'Yevhen Kariev'

--- 07_Spring_SpringBoot/Programming_Principles_DRY_YAGNI_KISS_SOLID/oopprinciples/Library.java
+++ 07_Spring_SpringBoot/Programming_Principles_DRY_YAGNI_KISS_SOLID/oopprinciples/Library.java
@@ -8,28 +8,27 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
-
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class Library {
-    private final static List<Book> books = new ArrayList<>();
-    private final static List<User> users = new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
 
-    static {
-        books.add(1, new Book("Maria sleeps", "John", "123-56", 5));
-        books.add(2, new Book("Racket up", "Smith", "148-56", 0));
-        books.add(3, new Book("London is true", "Igor", "559-46", 3));
-
-        users.add(1, new User("Vladimir", "01"));
+      {
+        books.add( new Book("Maria sleeps", "John", "123-56", 5));
+        books.add( new Book("Racket up", "Smith", "148-56", 0));
+        books.add( new Book("London is true", "Igor", "559-46", 3));
+        users.add( new User("Vladimir", "01"));
     }
+
 
     public void addBook(Book book) {
         books.add(book);
     }
 
     public void removeBook(Book book) {
-        if(book == null) {
+        if (book == null) {
             System.out.println("Book not founded.");
             return;
         } else {
@@ -42,9 +41,17 @@ public class Library {
         users.add(user);
     }
 
-    public static Book findBookByIsbn(String isbn){
+    public  Book findBookByIsbn(String isbn) {
         return books.stream()
                 .filter(book -> book.getIsbn()
                         .equals(isbn)).findFirst().orElse(null);
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "books=" + books +
+                ", users=" + users +
+                '}';
     }
 }
---
+++ 07_Spring_SpringBoot/Programming_Principles_DRY_YAGNI_KISS_SOLID/oopprinciples/ReflectionEx.java
@@ -0,0 +1,5 @@
+package oopprinciples;
+
+public class ReflectionEx {
+    
+}
