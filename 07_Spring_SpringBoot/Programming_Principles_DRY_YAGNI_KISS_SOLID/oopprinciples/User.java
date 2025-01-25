9ecb314a9a85fbbcbdc7e8d660beef8486574956
Yevhen Kariev <mediams@mail.ru>
2025-01-25 11:35:30 +0100

    25-01-2025 11:35 from PC 'Yevhen Kariev'

---
+++ 07_Spring_SpringBoot/Programming_Principles_DRY_YAGNI_KISS_SOLID/oopprinciples/User.java
@@ -0,0 +1,47 @@
+package oopprinciples;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class User {
+    public String name;
+    private String userId;
+    private List<Book> borrowedBooks = new ArrayList<>();
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(userId);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User{name='%s', userId='%s', borrowedBooks=%s}",
+                name, userId, borrowedBooks.isEmpty() ? "None" : borrowedBooks);
+    }
+
+}
