45022cd9211818a6ad96e9347b04da414cf19ef1
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-22 21:53:14 +0100

    methods

---
+++ src/main/java/a_oop/a_Syntax_Basics/operators/TaskEx.java
@@ -0,0 +1,18 @@
+package a_oop.a_Syntax_Basics.operators;
+
+public class TaskEx {
+    public static void main(String[] args) {
+        System.out.printf("Calculate position: %d", calculateHIghScopePosition(499));
+    }
+
+    private static int calculateHIghScopePosition(int scope) {
+        if (scope >= 1000) {
+            return 1;
+        } else if (scope >= 500) {
+            return 2;
+        } else if (scope >= 100) {
+            return 3;
+        }
+        return 4;
+    }
+}
