a96f4d064825a9b03594ce4df053c3d8b242a325
Yevhen Kariev <yevhenkariev@gmail.com>
2025-02-24 11:49:43 +0100

    tasks

---
+++ src/main/java/a_oop/a_Syntax_Basics/operators/EqualSumChecker.java
@@ -0,0 +1,15 @@
+package a_oop.a_Syntax_Basics.operators;
+
+public class EqualSumChecker {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));  //should return false since 1 + 1 is not equal to 1
+
+        System.out.println(hasEqualSum(1, 1, 2));  //should return true since 1 + 1 is equal to 2
+
+        System.out.println(hasEqualSum(1, -1, 0));  //should return true since 1 + (-1) is 1 - 1 and is equal to 0
+    }
+
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
+        return (num1 + num2) == num3;
+    }
+}
