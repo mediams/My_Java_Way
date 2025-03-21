3fe6a78e8200505aa84034d490a026afcdf57cfe
Yevhen Kariev <yevhenkariev@gmail.com>
2025-03-21 15:33:22 +0100

    Inheritance-3

--- src/main/java/a_oop/inheritance_encapsulation_polymorphism_and_abstraction/Dog.java
+++ src/main/java/a_oop/inheritance_encapsulation_polymorphism_and_abstraction/Dog.java
@@ -8,7 +8,11 @@ public class Dog extends Animal {
         super("Mutt", "Big", 500);
     }
 
-    public Dog(String type, String size, double weight, String earShape, String tailShape) {
+    public Dog(String type, double weight) {
+        this(type, weight, "Perky", "Curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
         super(type, weight < 15 ? "small" : (weight > 35 ? "medium" : "large"), weight);
         this.earShape = earShape;
         this.tailShape = tailShape;
@@ -20,5 +24,15 @@ public class Dog extends Animal {
                 "earShape='" + earShape + '\'' +
                 ", tailShape='" + tailShape + '\'' +
                 "} " + super.toString();
+    }
+
+    public void makeNoise() {
+
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        System.out.println("Dogs walk, run and wag their tail");
     }
 }
