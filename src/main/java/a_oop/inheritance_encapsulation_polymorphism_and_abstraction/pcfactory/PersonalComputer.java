46e978ad41d62c7abf10a05211423fae47a5e2f8
Yevhen Kariev <yevhenkariev@gmail.com>
2025-03-24 16:22:27 +0100

    Composition-2

---
+++ src/main/java/a_oop/inheritance_encapsulation_polymorphism_and_abstraction/pcfactory/PersonalComputer.java
@@ -0,0 +1,29 @@
+package a_oop.inheritance_encapsulation_polymorphism_and_abstraction.pcfactory;
+
+public class PersonalComputer extends Product {
+
+    private ComputerCase computerCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PersonalComputer(String model, String manufacturer,
+                            ComputerCase computerCase, Monitor monitor,
+                            Motherboard motherboard) {
+        super(model, manufacturer);
+        this.computerCase = computerCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public ComputerCase getComputerCase() {
+        return computerCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}
