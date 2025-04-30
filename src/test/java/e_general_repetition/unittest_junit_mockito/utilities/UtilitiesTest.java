b43a390b11fa5c285961bc72ea9a7e4b18ebbc36
Yevhen Kariev <yevhenkariev@gmail.com>
2025-04-30 12:35:56 +0200

    JUnit parameterized

--- src/test/java/e_general_repetition/unittest_junit_mockito/utilities/UtilitiesTest.java
+++ src/test/java/e_general_repetition/unittest_junit_mockito/utilities/UtilitiesTest.java
@@ -1,12 +1,20 @@
 package e_general_repetition.unittest_junit_mockito.utilities;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UtilitiesTest {
 
-    Utilities utilities = new Utilities();
+    private Utilities utilities;
+
+    @BeforeEach
+    void setUp() {
+        utilities = new Utilities();
+    }
 
     @Test
     void everyNthChar() {
@@ -26,9 +34,26 @@ class UtilitiesTest {
         assertNull(utilities.removePairs(null), "argument was nuul");
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "A, A",
+            "ZYZQB, ZYZQQB",
+            "123456, 12233445566",
+            "AB8EFG, AB88EFFG",
+            "ABCDEF, ABCDEF"
+    })
+    void removePairs_addingTests(String ex, String input) {
+        assertEquals(ex, utilities.removePairs(input));
+    }
+
     @Test
     void converter() {
         assertEquals(300, utilities.converter(10, 5));
+    }
+
+    @Test
+    void converter_divideByNull() {
+        assertThrows(ArithmeticException.class, () -> utilities.converter(10, 0));
     }
 
     @Test
