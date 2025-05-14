62e19e0e33799f40ef79db27df91d78e71e41db3
Yevhen Kariev <yevhenkariev@gmail.com>
2025-05-14 20:29:25 +0200

    JUnit Tests BinarySearchTaskTest

--- src/test/java/e_general_repetition/unittest_junit_mockito/junit/BinarySearchTaskTest.java
+++ src/test/java/e_general_repetition/unittest_junit_mockito/junit/BinarySearchTaskTest.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BinarySearchTaskTest {
 
     @Test
-    void search() {
+    void searchWithFoundData() {
         List<String> list = new ArrayList<>();
         list.add("A");
         list.add("B");
@@ -23,6 +24,38 @@ class BinarySearchTaskTest {
         assertEquals(2, c);
         assertEquals(0, BinarySearchTask.search(list, "A"));
         assertEquals(5, BinarySearchTask.search(list, "F"));
+
+        list = new ArrayList<>();
+        list.add("A");
+        assertEquals(0, BinarySearchTask.search(list, "A"));
+        assertEquals(-1, BinarySearchTask.search(list, "B"));
+    }
+
+    @Test
+    void searchWithNotFoundData() {
+        List<String> list = new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        list.add("F");
+
         assertEquals(-1, BinarySearchTask.search(list, "Y"));
+    }
+
+    @Test
+    void searchWithEmptyList() {
+        List<String> list = new ArrayList<>();
+
+        assertEquals(-1, BinarySearchTask.search(list, "Y"));
+    }
+
+    @Test
+    void searchWithIncorrectList() {
+        List<String> list = null;
+
+        assertThrows(NullPointerException.class, () -> BinarySearchTask.search(list, "A"));
+        assertThrows(NullPointerException.class, () -> BinarySearchTask.search(List.of("A", "B", "C"), null));
     }
 }
