cd5b04603f18ca1858dc84e8ff1d2689ab47c8b2
Yevhen Kariev <yevhenkariev@gmail.com>
2025-05-26 15:39:10 +0200

    varargs

---
+++ src/main/java/b_collections_framework/arrays/variable_arguments_varargs/Varargs.java
@@ -0,0 +1,25 @@
+package b_collections_framework.arrays.variable_arguments_varargs;
+
+public class Varargs {
+    public static void main(String... args) {
+        String[] strings = {"Hello", "World", "again"};
+        printText(strings);
+
+        System.out.println("-".repeat(30));
+        String string = "Hello";
+        printText(string);
+
+        System.out.println("-".repeat(30));
+        string = "Hello World again";
+        printText(string.split(" "));
+
+        System.out.println("-".repeat(30));
+        printText(String.join(" ", strings));
+    }
+
+    private static void printText(String... args) {
+        for (String s : args) {
+            System.out.println(s);
+        }
+    }
+}
