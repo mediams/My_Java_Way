eae202d392b7e9a50251eb2f5ca66fb33613b6d3
Yevhen Kariev <yevhenkariev@gmail.com>
2025-06-19 20:12:25 +0200

    List

---
+++ src/main/java/b_collections_framework/LinkedList_Implementation/MyArrayList.java
@@ -0,0 +1,31 @@
+package b_collections_framework.LinkedList_Implementation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+    public GroceryItem(String name) {
+        this(name, "DAIRY", 1);
+    }
+}
+
+public class MyArrayList {
+    public static void main(String[] args) {
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("apples2", "PRODUCE2", 12);
+
+        System.out.println(Arrays.toString(groceryArray));
+        for (Object o : groceryArray) {
+            System.out.println(o);
+        }
+
+        ArrayList objectList = new ArrayList<>();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("Yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+    }
+}
