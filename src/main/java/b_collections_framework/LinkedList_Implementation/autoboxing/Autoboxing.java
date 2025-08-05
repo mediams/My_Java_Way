80d2383b9311f7166ae219825f9e2ec7547177be
Yevhen Kariev <yevhenkariev@gmail.com>
2025-08-05 17:06:03 +0200

    Wrapper Autoboxing Lists

--- src/main/java/b_collections_framework/LinkedList_Implementation/autoboxing/Autoboxing.java
+++ src/main/java/b_collections_framework/LinkedList_Implementation/autoboxing/Autoboxing.java
@@ -1,5 +1,9 @@
 package b_collections_framework.LinkedList_Implementation.autoboxing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Autoboxing {
     public static void main(String[] args) {
         Integer exampleAutoboxing = Integer.valueOf(15);
@@ -14,7 +18,31 @@ public class Autoboxing {
         Double resultBoxed = getLiteralDoublePrimitive();
         double resultUnboxed = getLiteralDoublePrimitive();
 
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 50;
+        System.out.println(Arrays.toString(wrapperArray));
+        System.out.println(wrapperArray[0].getClass().getSimpleName());
 
+        Character[] characters = {'a', 'b', 'c', 'd'};
+        System.out.println(characters[0].getClass().getSimpleName());
+
+        var ourList = getList(1, 2, 3, 4);
+        System.out.println(ourList);
+
+    }
+
+    private static ArrayList<Integer> getList(Integer... varargs) {
+        ArrayList<Integer> aList = new ArrayList<>();
+        Collections.addAll(aList, varargs);
+        return aList;
+    }
+
+    private static int returnAnInt(Integer integer) {
+        return integer;
+    }
+
+    private static Integer returnAnInteger(int i) {
+        return i;
     }
 
     private static Double getDoubleObject() {
