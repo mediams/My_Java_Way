ae10a3dd4a0d69f1eb6ceb0d29db588b85b58f39
Yevhen Kariev <yevhenkariev@gmail.com>
2025-08-21 22:54:00 +0200

    Bank System customers

--- src/main/java/b_collections_framework/LinkedList_Implementation/banksystem/Customer.java
+++ src/main/java/b_collections_framework/LinkedList_Implementation/banksystem/Customer.java
@@ -3,14 +3,26 @@ package b_collections_framework.LinkedList_Implementation.banksystem;
 import java.util.ArrayList;
 import java.util.List;
 
-public record Customer(String name, ArrayList<Double> transactions) {
-    public Customer(String name, double transactions) {
+public class Customer {
+    private final String name;
+    private final ArrayList<Double> transactions;
+
+    public Customer(String name, double transaction) {
         this.name = name;
-        this.transactions = new ArrayList<>(List.of(transactions));
+        this.transactions = new ArrayList<>(List.of(transaction));
     }
 
     public void addTransaction(double transaction) {
         transactions.add(transaction);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Double> getTransactions() {
+        return transactions;
+    }
+
 }
+
